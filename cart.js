73619f465fff11bc25ea5d89e7822a5dcf04865e
Cole Hendershot <colehendershot@gmail.com>
2021-12-23 16:09:03 -0700

    Finished cart.js file

--- cart.js
+++ cart.js
@@ -3,68 +3,78 @@
 ///////////////////////////////////////////////
 /*
     In this file, you'll be writing code to
-    calculate order totals. You'll also be 
-    creating customer objects.  
+    calculate order totals. You'll also be
+    creating customer objects.
 */
 
 
 //////////////////PROBLEM 1////////////////////
-/*  
+/*
     Below is a cart array that has food objects
-    inside. 
+    inside.
 
     Write a callback below that uses the reduce
     array method to calculate the sum of all
-    the food. 
+    the food.
 */
 
 const cart = [
     {
-        name: 'pizza', 
+        name: 'pizza',
         price: 9.99
-    }, 
+    },
     {
-        name: 'pasta', 
+        name: 'pasta',
         price: 8.99
-    }, 
+    },
     {
-        name: 'salad', 
+        name: 'salad',
         price: 7.99
     }
 ]
 
 //CODE HERE
 
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
+const summedPrice = cart.reduce(function(accumulator, currElement, currIndex, wholeArray) {
+
+    return accumulator + currElement.price;
+
+}, 0)
+
+console.log(summedPrice);
 
 
 //////////////////PROBLEM 2////////////////////
-/*  
+/*
     Write a function called `calcFinalPrice` that
     can take in `cartTotal`,`couponValue`,
-    and `tax` arguments. 
+    and `tax` arguments.
 
-    Inside the function, calculate the tax 
+    Inside the function, calculate the tax
     on the cartTotal and add it in. Subtract
     the value of the coupon. Return the final
-    number. 
+    number.
 
     Note: the numbers passed in for `tax` will be
     decimals, for example: .06 for a 6% tax.
 */
 
 //CODE HERE
+const calcFinalPrice = function(cartTotal, couponValue, tax) {
+     cartTotal = cartTotal + (cartTotal * tax);
+     return cartTotal - couponValue;
+}
 
-
+//console.log(calcFinalPrice(100, 20, .06));
 
 //////////////////PROBLEM 3////////////////////
-/*  
-    In this problem, you'll create a model for 
+/*
+    In this problem, you'll create a model for
     a customer object as well as an example
-    object. 
+    object.
 
     Plan out a customer object for the cart page.
-    Think about the information that a 
+    Think about the information that a
     restaurant would need about its customers.
 
     In the TEXT ANSWER area below, describe the
@@ -72,14 +82,15 @@ const cart = [
     and why you chose those properties.
 
     Explain what data types each property should be
-    and why you chose those data types. 
+    and why you chose those data types.
 
-    Your object should have at least 4 properties. 
+    Your object should have at least 4 properties.
 */
 
 /*
     TEXT ANSWER HERE
-
+  My customer object will have the properties Names, Phone Numbers, Orders, and Card Payment Info. Names will have the data type
+  String along with orders. Numbers and Payment will both have the Data type of Number.
 */
 
 /*
@@ -88,3 +99,9 @@ const cart = [
 */
 
 //CODE HERE
+const customerObject = {
+    name: 'Steven Strigimor',
+    phoneNumber: 438126925,
+    cardInfo: 123543890234,
+    order: 'Beef Lasagna'
+};
