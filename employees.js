8572b5cbc91e9742da6e727f7d5c4479f63fb799
Cole Hendershot <colehendershot@gmail.com>
2021-12-23 16:28:06 -0700

    First Incomplete Submission

--- employees.js
+++ employees.js
@@ -2,17 +2,17 @@
 ///////////////////EMPLOYEES.JS/////////////////////
 ////////////////////////////////////////////////////
 /*
-    In this file, you'll be writing classes to 
+    In this file, you'll be writing classes to
     store information about restaurant employees.
 */
 
 //////////////////PROBLEM 1////////////////////
-/*  
+/*
     Create a new class called `Employee`.
-    Make sure to call your constructor, and 
+    Make sure to call your constructor, and
     require these 2 parameters: name, shifts.
 
-    Create a method on the class called 
+    Create a method on the class called
     `getSchedule` that prints a string:
     [NAME] works on [SHIFTS]
     Where NAME and SHIFTS reference the values
@@ -20,7 +20,16 @@
 */
 
 //CODE HERE
+class Employee {
+  constructor(name, shifts) {
+      this.name = name;
+      this.shifts = shifts;
+  }
 
+  getSchedule() {
+    console.log(`${this.name} works on ${this.shiifts}`);
+  }
+}
 
 
 /*
@@ -34,14 +43,14 @@
 */
 
 //CODE HERE
-
+const empOne = new Employee('Jess', 'weekday mornings, weekday afternoons');
 /*
     Call the `getSchedule` method on the
     `empOne` object.
 */
 
 //CODE HERE
-
+empOne.getSchedule();
 
 /*
     Make a copy of the empOne object
@@ -50,23 +59,24 @@
 
     Change the name of `empTwo` to 'Nick'.
 
-    Hint: you can do this inline with the 
-    spread operator or reassign it using 
+    Hint: you can do this inline with the
+    spread operator or reassign it using
     dot or bracket notation.
 */
 
 //CODE HERE
-
+let empTwo = {...empOne};
+let Nick = empTwo;
 
 
 //////////////////PROBLEM 2////////////////////
-/*  
-    Write a class called Manager that *extends* 
-    the Employee class. In the constructor, 
-    make sure you require all of the parameters 
-    from the Employee class as well as 1 
-    new one: employees, which will be an array of 
-    employees that report to this manager. 
+/*
+    Write a class called Manager that *extends*
+    the Employee class. In the constructor,
+    make sure you require all of the parameters
+    from the Employee class as well as 1
+    new one: employees, which will be an array of
+    employees that report to this manager.
     (Hint: don't forget to call the super function)
 
     Create a method called `getEmployees` that
@@ -78,8 +88,8 @@
     Create a second method called `addEmployee`
     that takes in one argument, `emp`, which
     should be a string of an employee name.
-    The function should push the name into 
-    the employees array. 
+    The function should push the name into
+    the employees array.
 */
 
 //CODE HERE
@@ -102,22 +112,22 @@
 
 /*
     Call the `getEmployees` method on the
-    `manager` object.  
+    `manager` object.
 */
 
 //CODE HERE
 
 /*
-    Call the `addEmployee` method on the 
-    `manager` object passing in the string 
+    Call the `addEmployee` method on the
+    `manager` object passing in the string
     'Coach' or whatever name you'd like.
 */
 
-//CODE HERE 
+//CODE HERE
 
 /*
     Call the `getEmployees` method on the
-    `manager` object again to confirm 
+    `manager` object again to confirm
     that an employee was added.
 */
 
